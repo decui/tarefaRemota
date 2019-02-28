4f2aac7dea282574ccf6b499f3ee4c2bf27a7963
Junior Henrique D. dos Santos <decui@live.com>
2019-02-27 21:44:18 -0300

    Adicionado: main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+     const char* Country1 = "Brasil";
+     const char* Country2 = "Deutschland";
+     const char* Country3 = "France";
+
+     printf("String: [%s] | Length: [%u]\n", Country1, strlen(Country1));
+     return 0;
+}
