8a5e029b9fc1a8e61cff9cbeb0e430d0b1e6806a
Junior Henrique D. dos Santos <decui@live.com>
2019-02-27 22:32:48 -0300

    Branch Desenvolvimento: Removido Paises em main.c

--- main.c
+++ main.c
@@ -4,8 +4,6 @@
 int main(void)
 {
      const char* Country1 = "Brasil";
-     const char* Country2 = "Deutschland";
-     const char* Country3 = "France";
 
      printf("String: [%s] | Length: [%u]\n", Country1, strlen(Country1));
      return 0;
